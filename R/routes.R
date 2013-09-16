621e1fc1a5fe3097bdbd92c7a0b64f54562e3d3e
Christoph Waldhauser <tophcito@gmail.com>
2013-09-16 11:45:52 +0200

    Random route finding ====================
    * Graphs can now be traversed from a to b following a random route.

--- R/routes.R
+++ R/routes.R
@@ -27,3 +27,47 @@ findRoute <- function(a, b, ntw) {
   res <- list(Vertices=resV, Edges=resE, Weight=resW)
   return(res)
 }
+
+##' find a random route from a to b in a given graph
+##' 
+##' This function uses a very simple heuristic to get from \code{a} to \code{b} 
+##' on a graph, returning the vertices and edges passed during the journey. For 
+##' a description of the heuristic, see Details.
+##' 
+##' The heuristic starts from \code{a}, and takes a random neighboring node. The
+##' selection probability depends on whether that node has already been visited.
+##' This search continues until the destination \code{b} or \code{maxiter} has 
+##' been reached.
+##' @param a,b vertix id for start and end point of the route
+##' @param gr \code{igraph}
+##' @param maxiter The maximum number of nodes to be visited. The target must be
+##'   reached within that limit, otherwise a warning will be issued. Defaults to
+##'   1000.
+##' @return A named list with the elements \code{v} containing the vertices and
+##'   \code{e} the edges visited during the journey.
+##' @export
+##' @examples
+##' data(vie)
+##' rt1 <- randomRoute(a=43, b=findV("Skodagasse", vie), gr=attr(vie, "graph"))
+randomRoute <- function(a, b, gr, maxiter=1000) {
+  actualV <- a
+  vertices <- actualV
+  edges <- NULL
+  i <- 0
+  while (actualV != b & i < maxiter) {
+    this.n <- neighbors(graph=gr, v=actualV, mode="out")
+    ## how often are they alreay contained within vertices
+    this.f <- sapply(as.list(this.n), function(x) sum(vertices==x))
+    this.p <- -1 * this.f + max(this.f)
+    if (all(this.p==0)) this.p <- this.p + 1
+    newV <- this.n[sample.int(length(this.n), 1, prob=this.p)]
+    newE <- E(gr, P=c(actualV, newV))
+    actualV <- newV
+    edges <- c(edges, newE)
+    vertices <- c(vertices, actualV)
+    i <- i + 1
+  }
+  if (i==maxiter) warning("Search did not reach target!")
+  res <- list(v=vertices, e=edges)
+  return(res)
+}
